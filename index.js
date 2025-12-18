f9f50ef04e3bd1a81d53009bd6fbf41a29632929
pablode <pablo.delapuente@gmail.com>
2025-12-18 11:41:40 -0300

    Agregar login seguro con bcrypt y limpieza de rutas

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+
+const bcrypt = require("bcrypt");
 const express = require("express");
 const app = express();
+
+const usuarioPrueba = {
+    usuario: "Jason",
+    password: "" // se llena con hash
+};
+
+// Generar hash al iniciar el servidor
+bcrypt.hash("Friday" , 10, (err,hash) => {
+    if (err) throw err;
+    usuarioPrueba.password = hash;
+    console.log("Hash generado para usuario de prueba", hash)
+});
 
 app.use(express.json());
 
@@ -12,7 +26,19 @@ app.listen(3000, () =>{
 });
 
 app.post("/login", (req , res) => {
-    console.log(req.body); 
-    console.log(req.body.usuario); 
-    res.send("datos recibidos");
+    const {usuario , password} = req.body;
+    
+    if (usuario !== usuarioPrueba.usuario) {
+        return res.status(401).json({mensage:"Usuario o contraseña incorrectos"});
+        } 
+        
+    bcrypt.compare(password, usuarioPrueba.password, (err, resultado) => {
+        if (err) return res.status(500).json({mensaje:"Error de servidor"});
+
+        if (resultado) {
+            return res.status(200).json({mensaje: "Login exitoso"});
+        } else {
+            return res.status(401).json({mensaje: "Usuario o contraseña incorrectos"});
+        }
+        });
 } );
