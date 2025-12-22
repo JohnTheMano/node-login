6a7557b1bd9dc1ca186b09f034ecbb971ef2ba8d
pablode <pablo.delapuente@gmail.com>
2025-12-22 11:02:19 -0300

    Login con JWT y roles de usuario

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const express = require("express");
+const jwt = require("jsonwebtoken");
 
+const JWT_SECRET = "mi_clave_super_secreta"; // luego poner en .env
 const app = express();
 app.use(express.json());
 
@@ -13,26 +15,30 @@ mongoose.connect("mongodb+srv://pablolito:moro@cluster0.lldz94w.mongodb.net/apre
 
 
 const usuarioSchema = mongoose.Schema({
-    usuario: {type : String, required:true, unique:true},
-    password: {type: String, required: true}
-})
+    usuario: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    role: { type: String, enum: ["staff", "profesor"], required: true }
+});
+
 
 const Usuario = mongoose.model("Usuario", usuarioSchema);
 
 
 
 // Generar hash al iniciar el servidor
-bcrypt.hash("Friday" , 10, async(err,hash) => {
+bcrypt.hash("Friday", 10, async (err, hash) => {
     if (err) throw err;
 
     const usuario = new Usuario({
-        usuario: "Jason",
-        password: hash // se llena con hash
+        usuario: "Staff1",
+        password: hash,
+        role: "staff"
     });
 
     await usuario.save().catch(err => console.log("usuario ya existe"));
-    console.log("usuario de prueba guardado en  MongoDB")
+    console.log("usuario de prueba guardado en MongoDB");
 });
+
 
 
 
@@ -59,10 +65,22 @@ app.post("/login", async (req, res) => {
         const resultado = await bcrypt.compare(password, user.password);
 
         if (resultado) {
-            return res.status(200).json({ mensaje: "Login exitoso" });
+            // Crear token JWT
+            const token = jwt.sign(
+                { id: user._id, usuario: user.usuario, role: user.role },
+                JWT_SECRET,
+                { expiresIn: "4h" } // duración del token
+            );
+
+            return res.status(200).json({
+                mensaje: "Login exitoso",
+                token,
+                role: user.role
+            });
         } else {
             return res.status(401).json({ mensaje: "Usuario o contraseña incorrectos" });
         }
+
 
     } catch (error) {
         return res.status(500).json({ mensaje: "Error de servidor" });
